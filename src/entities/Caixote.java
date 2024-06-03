c96d1dd5f8b8d22998d0837797d374b48ad19268
nslucas <lucasnunes.santos088@gmail.com>
2024-06-03 19:03:55 -0300

    refactor: melhorias no string builder

--- src/entities/Caixote.java
+++ src/entities/Caixote.java
@@ -1,5 +1,7 @@
 package entities;
 
+import java.text.DecimalFormat;
+
 public class Caixote {
 	private Double altura;
 	private Double largura;
@@ -8,7 +10,8 @@ public class Caixote {
 	private Integer espessuraFundo;
 	private Integer larguraBase;
 	private Integer larguraPorta;
-		
+	
+	private static final DecimalFormat df = new DecimalFormat("#");
 	public Caixote(Double altura, Double largura, Double profundidadeModulo, Integer espessura, Integer espessuraFundo) {
 		this.altura = altura;
 		this.largura = largura;
@@ -25,14 +28,30 @@ public class Caixote {
 	public void setLarguraPorta(Integer larguraPorta) {
 		this.larguraPorta = larguraPorta;
 	}
-	public String baseCaixote() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("2x {" + (largura - larguraBase) + "} " + "{" + profundidadeModulo + "} " + "{" + espessura + "}" );
-		return sb.toString();
+
+	public String formatarDimensoes(String prefixo, Double dim1, Double dim2, Integer dim3){
+		return new StringBuilder()
+		.append(prefixo)
+		.append(df.format(dim1))
+		.append(" * ")
+		.append(dim2)
+		.append(" * ")
+		.append(dim3)
+		.toString();
+	}
+
+
+	public String baseCaixote() { 
+		return formatarDimensoes("2x ", largura - larguraBase, altura, espessura);
 	}
 	public String lateralCaixote() {
 		StringBuilder sb = new StringBuilder();
-		sb.append("2x {" + altura + "}" + " {" + profundidadeModulo + "}" + "{" + espessura + "}" );
+		sb.append("2x " + df.format(altura) + " * " + df.format(profundidadeModulo) + " * " + espessura);
 		return sb.toString();
 	}
+
+	/* public String fundoCaixote(){
+		StringBuilder sb = new StringBuilder();
+		sb.append(0)
+	} */
 }
