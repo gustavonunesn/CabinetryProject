656bc5153f3fbd652506b5edfde93154c2e6cedf
nslucas <lucasnunes.santos088@gmail.com>
2024-06-03 20:10:27 -0300

    refactor: correção dos métodos baseCaixote e lateralCaixote

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,13 +7,15 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		Caixote c = new Caixote(1000.00,5000.00,500.00,15, 6);
+		Caixote c = new Caixote(1000.00,1000.00,500.00,15, 6);
 		
 		System.out.println(c.baseCaixote());
 		
 		c.setLarguraBase(5);
 		
 		System.out.println(c.baseCaixote());
+
+		System.out.println(c.lateralCaixote());
 
 		sc.close();
 
