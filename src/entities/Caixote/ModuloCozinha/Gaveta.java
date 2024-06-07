34667239befef59aae46dddd669131efe08aceda
Gustavo <gustavonoia51@gmail.com>
2024-06-07 11:23:22 -0300

    Finalizado a criação do Modulo e suas extensões

---
+++ src/entities/Caixote/ModuloCozinha/Gaveta.java
@@ -0,0 +1,36 @@
+package entities.Caixote.ModuloCozinha;
+
+import entities.Caixote.Modulo;
+
+public class Gaveta extends Modulo {
+    // Implementar o que esta peça possuirá.
+
+    public Gaveta(Double altura, Double largura, Double profundidade, Double espessura, Double espessuraFundo) {
+        super(altura, largura, profundidade, espessura, espessuraFundo);
+
+    }
+
+    public Double umaGaveta() {
+        return null;
+    }
+
+    public Double duasGavetas() {
+        return null;
+    }
+
+    public Double tresGavetas() {
+        return null;
+    }
+
+    public Double quatroGavetas() {
+        return null;
+    }
+
+    public Double gaveta2mais1() {
+        return null;
+    }
+
+    public Double portaTempeiros() {
+        return null;
+    }
+}
---
+++ src/entities/Caixote/ModuloCozinha/Portas.java
@@ -0,0 +1,12 @@
+package entities.Caixote.ModuloCozinha;
+
+import entities.Caixote.Modulo;
+
+public class Portas extends Modulo {
+    // Implementar o que esta peça possuirá.
+    public Portas(Double altura, Double largura, Double profundidade, Double espessura, Double espessuraFundo) {
+        super(altura, largura, profundidade, espessura, espessuraFundo);
+
+    }
+
+}
